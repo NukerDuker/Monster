3b8233fe5cc07b129adc09e2dba5246cfae27657
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2021-11-28 21:51:27 +0300

    adding Zombie methods

--- src/Monster.java
+++ src/Monster.java
@@ -1,17 +1,22 @@
 public class Monster {
+
     private String name;
     private int damage;
 
+    Monster () {
+        this("Dragon", 100);
+    }
     public Monster(String name, int damage) {
         this.name = name;
         this.damage = damage;
+        System.out.printf("%s was succesfully created!", this.name);
     }
 
-    public void growl (String growl){
+    public void growl(String growl) {
         System.out.println(growl);
     }
 
-    public void attack () {
+    public void attack() {
         System.out.println(damage);
     }
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,2 +1,13 @@
-public class Zombie {
+public class Zombie extends Monster {
+
+    private String growl;
+
+    public Zombie (String name) {
+        super(name, 5);
+        this.growl = "Raaaauuughhhh";
+    }
+
+    public void growl() {
+        System.out.println(growl);
+    }
 }
