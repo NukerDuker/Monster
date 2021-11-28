2412c87685437f9b6a59ff4460dc042d01f84905
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2021-11-28 21:33:16 +0300

    creating zombie clss

--- src/Monster.java
+++ src/Monster.java
@@ -1,2 +1,17 @@
 public class Monster {
+    private String name;
+    private int damage;
+
+    public Monster(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public void growl (String growl){
+        System.out.println(growl);
+    }
+
+    public void attack () {
+        System.out.println(damage);
+    }
 }
