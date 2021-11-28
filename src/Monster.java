99ecbeb6c7a5e15547fee5c6c6cb4df7c75bf9fe
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2021-11-28 22:49:26 +0300

    all classes complete

--- src/Monster.java
+++ src/Monster.java
@@ -3,20 +3,21 @@ public class Monster {
     private String name;
     private int damage;
 
-    Monster () {
+    Monster() {
         this("Dragon", 100);
     }
+
     public Monster(String name, int damage) {
         this.name = name;
         this.damage = damage;
-        System.out.printf("%s was succesfully created!", this.name);
+        System.out.printf("Monster %s was created!\n", this.name);
     }
 
-    public void growl(String growl) {
-        System.out.println(growl);
+    public void growl() {
+        System.out.printf("Monster %s growled\n", name);
     }
 
     public void attack() {
-        System.out.println(damage);
+        System.out.printf("Monster %s attacked with damage %d \n", name, damage);
     }
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -3,11 +3,16 @@ public class Zombie extends Monster {
     private String growl;
 
     public Zombie (String name) {
-        super(name, 5);
+        super(name + " the Zombie", 5);
         this.growl = "Raaaauuughhhh";
     }
 
     public void growl() {
-        System.out.println(growl);
+        System.out.print(growl);
+        super.growl();
+    }
+
+    public void attack () {
+        super.attack();
     }
 }
